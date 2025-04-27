729b6ac74213b111beb2236431bbd21ea6fb94ba
Ste Vaidis <ste.vaidis@gmail.com>
2025-04-27 21:50:19 +0300

    console debug messages

--- back/jwtTokens.ts
+++ back/jwtTokens.ts
@@ -4,17 +4,21 @@ const ACCESS_TOKEN_SECRET = 'your_super_secret_access_key';
 const REFRESH_TOKEN_SECRET = 'your_super_secret_refresh_key';
 
 export function generateAccessToken(payload: object) {
-  return jwt.sign(payload, ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
+  console.log('🍓 Generate new access token');
+  return jwt.sign(payload, ACCESS_TOKEN_SECRET, { expiresIn: '1m' });
 }
 
 export function generateRefreshToken(payload: object) {
+  console.log('🍓 Generate new refresh token');
   return jwt.sign(payload, REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
 }
 
 export function verifyAccessToken(payload: string) {
+  console.log('🍓 Verify access token');
   return jwt.verify(payload, ACCESS_TOKEN_SECRET);
 }
 
 export function verifyRefreshToken(payload: string) {
+  console.log('🍓 Verify refresh token');
   return jwt.verify(payload, REFRESH_TOKEN_SECRET);
 }
