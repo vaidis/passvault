f23f505aced74eb2fea32ca895eed3b0cb19400a
Ste Vaidis <ste.vaidis@gmail.com>
2025-05-23 18:05:37 +0300

    re-order routes

--- back/routes/authRoutes.ts
+++ back/routes/authRoutes.ts
@@ -4,8 +4,8 @@ import { login, refresh, user, logout } from '../controllers/authController';
 const router = express.Router();
 
 router.post('/login', login);
-router.post('/logout', logout);
 router.post('/refresh', refresh);
+router.get('/logout', logout);
 router.get('/user', user);
 
 export const authRouter = router;
