a14281ecdbe0ed0fb7f735405a95048bb01c7415
Ste Vaidis <ste.vaidis@gmail.com>
2025-06-11 14:20:04 +0300

    remove profile endpoint from auth endpoints

--- back/routes/authRoutes.ts
+++ back/routes/authRoutes.ts
@@ -6,6 +6,5 @@ const router = express.Router();
 router.post('/login', login);
 router.post('/refresh', refresh);
 router.get('/logout', logout);
-router.get('/user', user);
 
 export const authRouter = router;
