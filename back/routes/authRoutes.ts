907f1f4470f40c88604e17df3c632a5a619eadc9
Ste Vaidis <ste.vaidis@gmail.com>
2025-06-23 21:14:08 +0300

    clean up imports

--- back/routes/authRoutes.ts
+++ back/routes/authRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { login, refresh, user, logout } from '../controllers/authController';
+import { login, refresh, logout } from '../controllers/authController';
 
 const router = express.Router();
 
