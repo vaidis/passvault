042d4925cfc8050648eec63f3a5b7d95b9b6b7ae
Ste Vaidis <ste.vaidis@gmail.com>
2025-06-23 20:42:01 +0300

    types added

--- back/jwtTokens.ts
+++ back/jwtTokens.ts
@@ -1,24 +1,28 @@
 import jwt from 'jsonwebtoken';
 
-const ACCESS_TOKEN_SECRET = 'your_super_secret_access_key';
-const REFRESH_TOKEN_SECRET = 'your_super_secret_refresh_key';
+const ACCESS_TOKEN_SECRET = process.env.JWT_SECRET;
+const REFRESH_TOKEN_SECRET = process.env.JWT_SECRET;
+
+if (!ACCESS_TOKEN_SECRET || !REFRESH_TOKEN_SECRET) {
+  throw new Error('JWT secrets are not defined in environment variables');
+}
 
 export function generateAccessToken(payload: object) {
-  console.log('🍓 Generate new access token');
-  return jwt.sign(payload, ACCESS_TOKEN_SECRET, { expiresIn: '1m' });
+  console.log('🍓 jwtTokens.ts > Generate new access token');
+  return jwt.sign(payload, ACCESS_TOKEN_SECRET as string, { expiresIn: '5m' });
 }
 
 export function generateRefreshToken(payload: object) {
-  console.log('🍓 Generate new refresh token');
-  return jwt.sign(payload, REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
+  console.log('🍓 jwtTokens.ts > Generate new refresh token');
+  return jwt.sign(payload, REFRESH_TOKEN_SECRET as string, { expiresIn: '60m' });
 }
 
 export function verifyAccessToken(payload: string) {
-  console.log('🍓 Verify access token');
-  return jwt.verify(payload, ACCESS_TOKEN_SECRET);
+  console.log('🍓 jwtTokens.ts > Verify access token');
+  return jwt.verify(payload, ACCESS_TOKEN_SECRET as string);
 }
 
 export function verifyRefreshToken(payload: string) {
-  console.log('🍓 Verify refresh token');
-  return jwt.verify(payload, REFRESH_TOKEN_SECRET);
+  console.log('🍓 jwtTokens.ts > Verify refresh token');
+  return jwt.verify(payload, REFRESH_TOKEN_SECRET as string);
 }
